8156bfff93d2b170b6d71727f39b568482a9f18c
todoriva1 <todoriva@hotmail.com>
2020-08-20 23:39:25 -0400

    Initial commit

---
+++ src/Day44_Constructor/StudentObjects.java
@@ -0,0 +1,21 @@
+package Day44_Constructor;
+
+public class StudentObjects {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Ivan",41,'M'); // Student ()  is Constructor
+        Student student2 = new Student("Lepa",65,'F');
+        Student student3 = new Student("Daria",13,'F');
+
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
+
+
+
+
+
+
+    }
+}
